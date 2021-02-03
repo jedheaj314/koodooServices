b06cec34271e953639d4650de0dac42d20eed114
AJ <jedheaj3@gmail.com>
2021-02-03 02:16:47 +0000

    Added app files

---
+++ __tests__/balanceHistoryService.test.js
@@ -0,0 +1,137 @@
+import { accountTypeChecker } from '../balanceHistoryService'
+
+// SETUP
+// Type 🅰️
+// a balance history where the balance amount decreases by varying amounts each month.
+const typeABalancehistory = [
+  {
+    monthNumber: 0,
+    account: { balance: { amount: 600 } },
+  },
+  {
+    monthNumber: 1,
+    account: { balance: { amount: 400 } },
+  },
+  {
+    monthNumber: 2,
+    account: { balance: { amount: 5 } },
+  },
+]
+
+// Type 🅱️
+// where the balance amount changes by the same amount each month.
+const typeBBalancehistory = [
+  {
+    monthNumber: 0,
+    account: { balance: { amount: 50 } },
+  },
+  {
+    monthNumber: 1,
+    account: { balance: { amount: 100 } },
+  },
+  {
+    monthNumber: 2,
+    account: { balance: { amount: 150 } },
+  },
+  {
+    monthNumber: 3, // three months ago
+    account: { balance: { amount: 200 } },
+  },
+]
+
+const noneOfTheTypes = [
+  {
+    monthNumber: 3,
+    account: { balance: { amount: 10 } },
+  },
+  {
+    monthNumber: 2,
+    account: { balance: { amount: 1 } },
+  },
+  {
+    monthNumber: 1,
+    account: { balance: { amount: 6 } },
+  },
+  {
+    monthNumber: 0,
+    account: { balance: { amount: 3 } },
+  },
+]
+
+const unsorted = [
+  {
+    monthNumber: 0,
+    account: { balance: { amount: 10 } },
+  },
+  {
+    monthNumber: 2,
+    account: { balance: { amount: 3 } },
+  },
+  {
+    monthNumber: 1,
+    account: { balance: { amount: 6 } },
+  },
+  {
+    monthNumber: 3,
+    account: { balance: { amount: 2 } },
+  },
+]
+
+// HELPER
+const mapBalanceHistoryInput = input => (
+  input
+    .map(b => ({ monthNumber: b.monthNumber, account: { balance: { amount: b.amount } } })))
+
+// TEST
+describe('Balance hostory tests', () => {
+  test.each([
+    [[]],
+    [[{ monthNumber: 0, amount: 200 }]],
+    [[{ monthNumber: 0, amount: 200 }, { monthNumber: 1, amount: 400 }]],
+  ])('Given invalid account balance history accountTypeChecker returns null', (balanceList) => {
+    const invalidBalanceHistory = mapBalanceHistoryInput(balanceList)
+    const result = accountTypeChecker(invalidBalanceHistory)
+    expect(result).toEqual(null)
+  })
+
+  test.each([
+    [null],
+    ['invalid'],
+  ])('Given invalid type of account balance history accountTypeChecker returns null', (invalidBalance) => {
+    const result = accountTypeChecker(invalidBalance)
+    expect(result).toEqual(null)
+  })
+
+  test('scenario 1: Given decreased with varying change accoutTypeChecker returns A.', () => {
+    const result = accountTypeChecker(typeABalancehistory)
+    expect(result).toEqual('A')
+  })
+
+  test('scenario 1: Given same changed balance each month accoutTypeChecker returns B.', () => {
+    const result = accountTypeChecker(typeBBalancehistory)
+    expect(result).toEqual('B')
+  })
+
+  test('scenario 1: Given balance increased and  different change each month accoutTypeChecker returns C.', () => {
+    const result = accountTypeChecker(noneOfTheTypes)
+    expect(result).toEqual('C')
+  })
+
+  test('scenario 1: Given unsorted  balance history accoutTypeChecker returns expected result.', () => {
+    const result = accountTypeChecker(unsorted)
+    expect(result).toEqual('A')
+  })
+
+  test.each([
+    [[{ monthNumber: 0, amount: 200 }, { monthNumber: 1, amount: 400 },
+      { monthNumber: 2, amount: 600 }, { monthNumber: 3, amount: 400 }]],
+    [[{ monthNumber: 0, amount: 600 }, { monthNumber: 1, amount: 400 },
+      { monthNumber: 2, amount: 200 }, { monthNumber: 3, amount: 0 }]],
+    [[{ monthNumber: 0, amount: 2 }, { monthNumber: 1, amount: 4 },
+      { monthNumber: 2, amount: 2 }, { monthNumber: 3, amount: 4 }]],
+  ])('Given balance increased and decreased with same change accountTypeChecker returns B', (balanceList) => {
+    const invalidBalanceHistory = mapBalanceHistoryInput(balanceList)
+    const result = accountTypeChecker(invalidBalanceHistory)
+    expect(result).toEqual('B')
+  })
+})
---
+++ balanceHistoryService.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-console */
+/** *
+  Type 🅰️
+  denotes a balance history where the balance amount decreases by varying amounts each month.
+  - decreasedByVaryingAmount
+
+  Type 🅱️
+  is one where the balance amount changes by the same amount each month.
+  - changedBySameAmount
+  ** */
+
+const getChangeAmount = (a, b) => (a > b ? a - b : b - a)
+
+const decreasedByVaryingAmount = (decreasedByVaryingAmounts, previousBalance, currentBalance) => {
+  if (decreasedByVaryingAmounts) {
+    if (previousBalance < currentBalance) {
+      return false
+    }
+  }
+
+  return decreasedByVaryingAmounts
+}
+
+const mapResult = (decreasedByVaryingAmounts, changedBySameAmount) => {
+  if (decreasedByVaryingAmounts) {
+    return 'A'
+  } if (changedBySameAmount) {
+    return 'B'
+  }
+
+  return 'C'
+}
+
+const getType = (accountBalanceHistory) => {
+  const amountChanges = []
+  let i = 1
+  let decreasedByVaryingAmounts = true
+  let changedBySameAmount = true
+  while (i < accountBalanceHistory.length) {
+    const currentBalance = accountBalanceHistory[i].account.balance.amount
+    const previousBalance = accountBalanceHistory[i - 1].account.balance.amount
+
+    decreasedByVaryingAmounts = decreasedByVaryingAmount(
+      decreasedByVaryingAmounts,
+      previousBalance,
+      currentBalance,
+    )
+
+    const changeAmount = getChangeAmount(previousBalance, currentBalance)
+    if (amountChanges.length > 0) {
+      const previousChange = amountChanges[amountChanges.length - 1]
+      if (changeAmount !== previousChange) {
+        changedBySameAmount = false
+      } else {
+        decreasedByVaryingAmounts = false
+      }
+
+      if (!decreasedByVaryingAmounts && !changedBySameAmount) {
+        return 'C'
+      }
+    }
+
+    amountChanges.push(changeAmount)
+    i += 1
+  }
+
+  return mapResult(decreasedByVaryingAmounts, changedBySameAmount)
+}
+
+export const accountTypeChecker = (accountBalanceHistory) => {
+  if (!accountBalanceHistory
+    || !Array.isArray(accountBalanceHistory)
+    || accountBalanceHistory.length < 3) return null
+
+  accountBalanceHistory
+    .sort((m1, m2) => (m1.monthNumber > m2.monthNumber ? 1 : -1))
+
+  return getType(accountBalanceHistory)
+}
